ca1c518ae1e69ab4c6b2d24fbc785d4275864c21
sanjayraoms <sanjayraoms@gmail.com>
2024-05-23 23:30:46 +0530

    Singleton Design Pattern

---
+++ SingletonPattern/Singleton.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    public class Singleton
+    {
+        private static Singleton singletonInstance;
+
+        public static readonly object SingletonLock = new object();
+
+        private Singleton()
+        {
+
+        }
+
+        public static Singleton GetSingletonInstance()
+        {
+            if (singletonInstance == null)
+            {
+                lock (SingletonLock)
+                {
+                    if (singletonInstance == null)
+                    {
+                        singletonInstance = new Singleton();
+                    }
+                }
+            }
+            return singletonInstance;
+        }
+    }
+}
