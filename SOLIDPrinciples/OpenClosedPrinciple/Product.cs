e5f76943504610cf4427863acb5402fb8b0d7bf4
sanjayraoms <sanjayraoms@gmail.com>
2024-07-03 12:47:06 +0530

    Open and Closed Principle

---
+++ SOLIDPrinciples/OpenClosedPrinciple/Product.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLIDPrinciples.OpenClosedPrinciple
+{
+    public enum Color
+    {
+        GREEN, YELLOW, RED
+    }
+    public enum Size
+    {
+        SMALL, MEDIUM, LARGE
+    }
+    public class Product
+    {
+        public string Name { get; set; }
+        public Color Color { get; set; }
+        public Size Size { get; set; }
+
+        public Product(string name, Color color, Size size)
+        {
+            Name = name;
+            Color = color;
+            Size = size;
+        }
+    }
+
+    public class ProductFilter
+    {
+        // This Below Filtering patterns violates Open Closed Principle
+        public IEnumerable<Product> FilterByColor(IEnumerable<Product> products,Color color)
+        {
+            foreach (var item in products)
+            {
+                if(item.Color == color)
+                    yield return item;
+            }
+        }
+
+        public IEnumerable<Product> FilterBySize(IEnumerable<Product> products, Size size)
+        {
+            foreach (var item in products)
+            {
+                if (item.Size == size)
+                    yield return item;
+            }
+        }
+
+        public IEnumerable<Product> FilterBySizeandColor(IEnumerable<Product> products, Size size, Color color)
+        {
+            foreach (var item in products)
+            {
+                if (item.Size == size && item.Color == color)
+                    yield return item;
+            }
+        }
+
+    }
+
+    public interface ISpecification<T>
+    {
+        bool IsSatisfied(Product product);
+    }
+
+    public interface IFilter<T>
+    {
+        IEnumerable<T> Filter(IEnumerable<T> items, ISpecification<T> spec);
+    }
+
+    public class ColorSpec : ISpecification<Product>
+    {
+        private Color color;
+        public ColorSpec(Color color)
+        {
+            this.color = color;
+        }
+        public bool IsSatisfied(Product product)
+        {
+            return product.Color == color;
+        }
+    }
+
+    public class SizeSpec : ISpecification<Product>
+    {
+        private Size size;
+        public SizeSpec(Size size)
+        {
+            this.size = size;
+        }
+
+        public bool IsSatisfied(Product product)
+        {
+            return product.Size == size;
+        }
+    }
+
+    public class AndSpecification<T> : ISpecification<T>
+    {
+        private ISpecification<T> first,second;
+
+        public AndSpecification(ISpecification<T> first, ISpecification<T> second)
+        {
+            this.first = first;
+            this.second = second;
+        }
+
+        public bool IsSatisfied(Product product)
+        {
+            return first.IsSatisfied(product) && second.IsSatisfied(product);
+        }
+    }
+    public class BetterFilter : IFilter<Product>
+    {
+        // Closed for Extension
+        public IEnumerable<Product> Filter(IEnumerable<Product> products, ISpecification<Product> spec)
+        {
+            foreach (var item in products)
+            {
+                if (spec.IsSatisfied(item))
+                   yield return item;
+            }
+        }
+    }
+}
--- SOLIDPrinciples/Program.cs
+++ SOLIDPrinciples/Program.cs
@@ -1,20 +1,59 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using SOLIDPrinciples.OpenClosedPrinciple;
 using SOLIDPrinciples.SingleResponsibility;
 using System.Diagnostics;
+using static System.Console;
 
 Console.WriteLine("Hello, World!");
 
-Journal Journal = new Journal();
-Journal.AddEntry("Today is a good day");
-Journal.AddEntry("Discipline everyday");
+// Single Responsibility
+//Journal Journal = new Journal();
+//Journal.AddEntry("Today is a good day");
+//Journal.AddEntry("Discipline everyday");
 
-Persistence Persistence = new Persistence();
-string filename = @"C:\Temp\SRP.txt";
-Persistence.SavetoFile(Journal, filename, true);
+//Persistence Persistence = new Persistence();
+//string filename = @"C:\Temp\SRP.txt";
+//Persistence.SavetoFile(Journal, filename, true);
 
-var psi = new ProcessStartInfo(filename)
+//var psi = new ProcessStartInfo(filename)
+//{
+//    UseShellExecute = true
+//};
+
+//Process.Start(psi);
+
+// Open Closed Principle
+
+var apple = new Product("Apple", Color.GREEN, Size.SMALL);
+var tree = new Product("Tree", Color.GREEN, Size.LARGE);
+var house = new Product("House", Color.RED, Size.LARGE);
+
+Product[] products = { apple, tree, house };
+
+var pf = new ProductFilter();
+WriteLine("Green products (old):");
+foreach (var p in pf.FilterByColor(products, Color.GREEN))
+    WriteLine($" - {p.Name} is green");
+
+// ^^ BEFORE
+
+// vv AFTER
+var bf = new BetterFilter();
+WriteLine("Green products (new):");
+foreach (var p in bf.Filter(products,new ColorSpec(Color.GREEN)))
+    WriteLine($" - {p.Name} is green");
+
+WriteLine("Large products");
+foreach (var p in bf.Filter(products, new SizeSpec(Size.LARGE)))
+    WriteLine($" - {p.Name} is large");
+
+WriteLine("Large blue items");
+foreach (var p in bf.Filter(products,
+  new AndSpecification<Product>(new ColorSpec(Color.RED), new SizeSpec(Size.LARGE)))
+)
 {
-    UseShellExecute = true
-};
+    WriteLine($" - {p.Name} is big and blue");
+}
 
-Process.Start(psi);
+ReadLine();
+
