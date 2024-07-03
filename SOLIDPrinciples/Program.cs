2826529c4d0ea6c5732c3b0285bd645fbb007345
sanjayraoms <sanjayraoms@gmail.com>
2024-07-03 10:52:17 +0530

    Single Responsibility Principle

---
+++ SOLIDPrinciples/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SOLIDPrinciples.SingleResponsibility;
+using System.Diagnostics;
+
+Console.WriteLine("Hello, World!");
+
+Journal Journal = new Journal();
+Journal.AddEntry("Today is a good day");
+Journal.AddEntry("Discipline everyday");
+
+Persistence Persistence = new Persistence();
+string filename = @"C:\Temp\SRP.txt";
+Persistence.SavetoFile(Journal, filename, true);
+
+var psi = new ProcessStartInfo(filename)
+{
+    UseShellExecute = true
+};
+
+Process.Start(psi);
---
+++ SOLIDPrinciples/SingleResponsibility/Journal.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLIDPrinciples.SingleResponsibility
+{
+    public class Journal
+    {
+        private readonly List<string> entries = new List<string>();
+        private static int count;
+
+        public int AddEntry(string text)
+        {
+            entries.Add($"{++count}:{text}");
+            return count;
+        }
+
+        public void RemoveEntry(int index)
+        {
+            entries.RemoveAt(index);
+        }
+
+        public override string ToString()
+        {
+            return string.Join(Environment.NewLine, entries);
+        }
+
+        //Violates Single Responsibility principle as Journal class is adding entries and saving entries
+        //public void Save(string filename,bool overwrite = false)
+        //{
+        //    File.WriteAllText(filename,ToString());
+        //}
+    }
+
+    public class Persistence
+    {
+        public void SavetoFile(Journal journal, string filename, bool overwrite = false)
+        {
+            if (overwrite)
+            {
+                File.WriteAllText(filename, journal.ToString()); 
+            }
+        }
+    }
+}
