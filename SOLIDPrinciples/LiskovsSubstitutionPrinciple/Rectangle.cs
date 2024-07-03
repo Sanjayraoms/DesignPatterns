29da174390f61b3bafd3f1bb81a879b19db204ae
sanjayraoms <sanjayraoms@gmail.com>
2024-07-03 16:53:49 +0530

    Liskov's Substitution principle.

---
+++ SOLIDPrinciples/LiskovsSubstitutionPrinciple/Rectangle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLIDPrinciples.LiskovsSubstitutionPrinciple
+{
+    public class Rectangle
+    {
+        public virtual int Width { get; set; }
+
+        public virtual int Height { get; set; }
+
+        public Rectangle()
+        {
+
+        }
+        public Rectangle(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Width)}:{Width},{nameof(Height)}:{Height}";
+        }
+    }
+
+    public class Square : Rectangle
+    {
+        override public int Width { set { base.Width = base.Height = value; } }
+
+        public override int Height { set => base.Width = base.Height = value; }
+
+    }
+}
--- SOLIDPrinciples/Program.cs
+++ SOLIDPrinciples/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using SOLIDPrinciples.LiskovsSubstitutionPrinciple;
 using SOLIDPrinciples.OpenClosedPrinciple;
 using SOLIDPrinciples.SingleResponsibility;
 using System.Diagnostics;
@@ -24,36 +25,46 @@ Console.WriteLine("Hello, World!");
 
 // Open Closed Principle
 
-var apple = new Product("Apple", Color.GREEN, Size.SMALL);
-var tree = new Product("Tree", Color.GREEN, Size.LARGE);
-var house = new Product("House", Color.RED, Size.LARGE);
+//var apple = new Product("Apple", Color.GREEN, Size.SMALL);
+//var tree = new Product("Tree", Color.GREEN, Size.LARGE);
+//var house = new Product("House", Color.RED, Size.LARGE);
 
-Product[] products = { apple, tree, house };
+//Product[] products = { apple, tree, house };
 
-var pf = new ProductFilter();
-WriteLine("Green products (old):");
-foreach (var p in pf.FilterByColor(products, Color.GREEN))
-    WriteLine($" - {p.Name} is green");
+//var pf = new ProductFilter();
+//WriteLine("Green products (old):");
+//foreach (var p in pf.FilterByColor(products, Color.GREEN))
+//    WriteLine($" - {p.Name} is green");
 
-// ^^ BEFORE
+//// ^^ BEFORE
 
-// vv AFTER
-var bf = new BetterFilter();
-WriteLine("Green products (new):");
-foreach (var p in bf.Filter(products,new ColorSpec(Color.GREEN)))
-    WriteLine($" - {p.Name} is green");
+//// vv AFTER
+//var bf = new BetterFilter();
+//WriteLine("Green products (new):");
+//foreach (var p in bf.Filter(products,new ColorSpec(Color.GREEN)))
+//    WriteLine($" - {p.Name} is green");
 
-WriteLine("Large products");
-foreach (var p in bf.Filter(products, new SizeSpec(Size.LARGE)))
-    WriteLine($" - {p.Name} is large");
+//WriteLine("Large products");
+//foreach (var p in bf.Filter(products, new SizeSpec(Size.LARGE)))
+//    WriteLine($" - {p.Name} is large");
 
-WriteLine("Large blue items");
-foreach (var p in bf.Filter(products,
-  new AndSpecification<Product>(new ColorSpec(Color.RED), new SizeSpec(Size.LARGE)))
-)
-{
-    WriteLine($" - {p.Name} is big and blue");
-}
+//WriteLine("Large blue items");
+//foreach (var p in bf.Filter(products,
+//  new AndSpecification<Product>(new ColorSpec(Color.RED), new SizeSpec(Size.LARGE)))
+//)
+//{
+//    WriteLine($" - {p.Name} is big and blue");
+//}
 
+//Liskov's Substitution principle
+
+static int Area(Rectangle rectangle) => rectangle.Width * rectangle.Height;
+
+Rectangle rectangle = new Rectangle(2,3);
+WriteLine($"{rectangle} has area {Area(rectangle)}");
+
+Rectangle square = new Square();
+square.Width = 2;
+WriteLine($"{square} has area {Area(square)}");
 ReadLine();
 
