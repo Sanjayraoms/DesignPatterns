70a9f81ba588635da5d77ba9cf330e5a18030deb
sanjayraoms <sanjayraoms@gmail.com>
2024-07-25 05:39:29 +0530

    Builder pattern : Fluent builder

---
+++ BuilderPatterb/HTMLElement.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuilderPattern
+{
+    public class HTMLElement
+    {
+        public string Name, Text;
+        public List<HTMLElement> Elements = new List<HTMLElement> ();
+        public const int indentSize = 2;
+
+        public HTMLElement()
+        {
+
+        }
+
+        public HTMLElement(string name, string text)
+        {
+            Name = name ?? throw new ArgumentNullException (paramName:nameof(text));
+            Text = text ?? throw new ArgumentNullException (paramName:nameof(text));
+        }
+
+        private string ToStringImpl(int indent)
+        {
+            var sb = new StringBuilder ();
+            var i = new string (' ', indentSize * indent);
+            sb.AppendLine($"{i}<{Name}>");
+            if (!string.IsNullOrEmpty(Text))
+            {
+                sb.Append(new string(' ', indentSize * (indent + 1)));
+                sb.AppendLine(Text);
+            }
+
+            foreach (var e in Elements)
+            {
+                sb.Append(e.ToStringImpl(indent + 1));
+            }
+            sb.AppendLine($"{i}</{Name}>");
+            return sb.ToString ();
+
+        }
+
+        public override string ToString()
+        {
+            return ToStringImpl(0);
+        }
+    }
+
+    public class HTMLBuilder
+    {
+        private readonly string rootName;
+        HTMLElement root = new HTMLElement();
+
+        public HTMLBuilder(string rootName)
+        {
+            this.rootName = rootName;
+            root.Name = rootName;
+        }
+
+        public HTMLBuilder AddChild(string childName, string childText)
+        {
+            var e = new HTMLElement(childName, childText);
+            root.Elements.Add(e);
+            return this;
+        }
+
+        public override string ToString()
+        {
+            return root.ToString();
+        }
+
+        public void Clear()
+        {
+            root = new HTMLElement { Name = rootName };
+        }
+    }
+}
---
+++ BuilderPatterb/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BuilderPattern;
+
+Console.WriteLine("Hello, World!");
+
+var builder = new HTMLBuilder("ul");
+builder.AddChild("li", "Hello").AddChild("li", "World!!");
+
+Console.WriteLine(builder.ToString());
+
+Console.Read();
