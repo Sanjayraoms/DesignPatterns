a0827b7ac93e2f483778ae5d7c43534363aeab6f
sanjayraoms <sanjayraoms@gmail.com>
2024-08-02 04:26:53 +0530

    Asynchronous Factory

---
+++ AsynchronousFactory/Foo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsynchronousFactory
+{
+    public class Foo
+    {
+        private Foo()
+        {
+
+        }
+
+        private async Task<Foo> InitAsync()
+        {
+            await Task.Delay(1000);
+            return this;
+        }
+
+        public static Task<Foo> CreateAsync()
+        {
+            var foo = new Foo();
+            return foo.InitAsync();
+        }
+    }
+}
---
+++ AsynchronousFactory/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AsynchronousFactory;
+
+Console.WriteLine("Hello, World!");
+
+Foo x = await Foo.CreateAsync();
