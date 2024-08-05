7db9c1d19dc097214550e85b64a64f14cd540ab9
sanjayraoms <sanjayraoms@gmail.com>
2024-08-05 05:42:28 +0530

    StrategyPattern example implementation

---
+++ StrategyPattern/Duck.cs
@@ -0,0 +1,31 @@
+﻿using StrategyPattern.FlyBehavior;
+using StrategyPattern.QuackBehavior;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyPattern
+{
+    public abstract class Duck
+    {
+        public IFlyBehavior FlyBehavior;
+        public IQuackBehavior QuackBehavior;
+
+        public void PerformFly()
+        {
+            FlyBehavior.Fly();
+        }
+
+        public void PerformQuack()
+        {
+            QuackBehavior.Quack();
+        }
+
+        public void Swim()
+        {
+            Console.WriteLine("All ducks can swim : Even Rubber and Decoy duck");
+        }
+    }
+}
