3483cc3fd296f2f22c9474ee609f59ef343aa3d9
sanjayraoms <sanjayraoms@gmail.com>
2024-08-10 12:38:58 +0530

    Added a new observer for the existing program.

--- ObserverPattern/Program.cs
+++ ObserverPattern/Program.cs
@@ -7,6 +7,8 @@ Console.WriteLine("Hello, World!");
 WeatherDataObject WeatherDataObject = new WeatherDataObject();
 
 CurrentConditionDisplay currentCondition = new CurrentConditionDisplay(WeatherDataObject);
+StatisticsDisplay statisticsDisplay = new StatisticsDisplay(WeatherDataObject);
 
 WeatherDataObject.setMeasurements(81.5f, 65, 30.4f);
-
+WeatherDataObject.setMeasurements(85f, 67, 35.4f);
+WeatherDataObject.setMeasurements(85f, 67, 35.4f);
---
+++ ObserverPattern/Subscribers/StatisticsDisplay.cs
@@ -0,0 +1,30 @@
+﻿using ObserverPattern.Publisher;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObserverPattern.Subscribers
+{
+    public class StatisticsDisplay : IObserver, IDisplayElement
+    {
+        private WeatherDataObject weatherDataObject;
+        private List<float> temperatures = new List<float>();
+        public StatisticsDisplay(WeatherDataObject weatherDataObject)
+        {
+            this.weatherDataObject = weatherDataObject;
+            this.weatherDataObject.addSubscribers(this);
+        }
+        public void Display()
+        {
+            Console.WriteLine($"Avg/Max/Min Temperatures : {temperatures.Average()}F / {temperatures.Max()}F / {temperatures.Min()}F");
+        }
+
+        public void Update()
+        {
+            temperatures.Add(weatherDataObject.getTemperature());
+            Display();
+        }
+    }
+}
