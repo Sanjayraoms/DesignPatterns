242d34dc1a5fdec0e64ea254130713d191e467c5
sanjayraoms <sanjayraoms@gmail.com>
2024-08-18 19:02:25 +0530

    Singleton Design Pattern

---
+++ HFSingletonPattern/Singleton.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HFSingletonPattern
+{
+    public class Singleton
+    {
+        private static Singleton uniqueSingleton;
+
+        public static readonly object SingletonLock = new object();
+        private Singleton()
+        {
+        }
+
+        public static Singleton GetSingleton()
+        {
+            if (uniqueSingleton == null)
+            {
+                lock (SingletonLock)
+                {
+                    if (uniqueSingleton == null)
+                        uniqueSingleton = new Singleton();
+                }
+            }
+
+            return uniqueSingleton;
+        }
+    }
+}
